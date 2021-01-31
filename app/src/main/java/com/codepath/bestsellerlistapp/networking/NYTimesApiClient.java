909d954360306fd088f9573e0027f12b42bb7f15
timmypass17 <timmypass17@gmail.com>
2021-01-30 17:14:01 -0800

    Update the developer api key for NYTimes

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "o5EbER78gVIy9vWPuEAqK9QQIzqYMSbN";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
